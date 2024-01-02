4365038eb47309759fc8e20f5a2aaaada67572f0
Wilfried Musanzi <musanziw.m@gmail.com>
2024-01-02 13:12:50 +0200

    style: use chadcn tabs and create a map for skeleton

--- app/(pages)/me/page.tsx
+++ app/(pages)/me/page.tsx
@@ -2,28 +2,30 @@
 
 import Topbar from "@/app/shared/components/Topbar";
 import axios from "@/app/shared/config/axios";
-import { useEffect, useMemo, useState } from "react";
-import { useSession } from "next-auth/react";
+import {useEffect, useMemo, useState} from "react";
+import {useSession} from "next-auth/react";
 import Image from 'next/image'
-import { ToastContainer } from "react-toastify";
+import {ToastContainer} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { moment } from "@/app/shared/config/moment"
-import { FilePond } from 'react-filepond'
+import {moment} from "@/app/shared/config/moment"
+import {FilePond} from 'react-filepond'
 import 'filepond/dist/filepond.min.css'
-import { API_BASE_URL } from "@/app/shared/config/urls";
+import {API_BASE_URL} from "@/app/shared/config/urls";
 import SolutionCard from "@/app/(pages)/me/components/SolutionCard";
 import UpdateProfile from "@/app/(pages)/me/components/UpdateProfile";
-import { Skeleton } from "@/app/shared/utils/ui/skeleton";
+import {Skeleton} from "@/app/shared/utils/ui/skeleton";
+import {Tabs, TabsContent, TabsList} from "@/app/shared/utils/ui/tabs";
+import {TabsTrigger} from "@radix-ui/react-tabs";
 
 moment.locale('fr')
 
 export default function Solution() {
-    const { data: account, status: authStatus } = useSession()
+    const {data: account, status: authStatus} = useSession()
     const [solutions, setSolutions] = useState<any>()
     const [user, setUser] = useState<any>()
     const [active, setActive] = useState<string>('profile')
     const [files, setFiles] = useState<any>([])
-    const authenticated = authStatus === 'authenticated'
+    const authenticated = useMemo(() => authStatus === 'authenticated', [authStatus])
     const imgPath: string = useMemo(() => `${API_BASE_URL}uploads/${user?.profile}`, [user])
 
     useEffect(() => {
@@ -35,9 +37,9 @@ export default function Solution() {
                 profile: account?.user?.image
             }
             await axios.post(`users/create`, JSON.stringify(payload))
-            const { data: response } = await axios.get(`auth/profile/${account?.user?.email}`)
+            const {data: response} = await axios.get(`auth/profile/${account?.user?.email}`)
             setUser(response.data)
-            const { data: reponse } = await axios.get(`solutions/user/${account?.user?.email}`)
+            const {data: reponse} = await axios.get(`solutions/user/${account?.user?.email}`)
             setSolutions(reponse.data)
         })()
     }, [account, authenticated])
@@ -46,7 +48,7 @@ export default function Solution() {
     return (
         <>
             <div className={'relative'}>
-                <Topbar />
+                <Topbar/>
                 <div
                     className={`p-8 flex flex-col mx-6 justify-start max-w-screen-lg md:mx-auto border-x border-dashed`}>
                     {
@@ -84,74 +86,60 @@ export default function Solution() {
                                     </div>
                                 </div>
 
-                                <div className="flex gap-5 mb-6">
-                                    <button
-                                        className={`pb-1 after:block after:h-[2px]  ${active === 'profile' && 'after:bg-indigo-400'}`}
-                                        onClick={() => setActive('profile')}>
-                                        Mon profil
-                                    </button>
-                                    {
-                                        solutions?.length > 0 && (
-                                            <button
-                                                className={`pb-1 after:block after:h-[2px]  ${active === 'solutions' && 'after:bg-indigo-400'}`}
-                                                onClick={() => setActive('solutions')}>
-                                                Mes solutions
-                                            </button>
-                                        )
-                                    }
-                                </div>
-
-                                <div className="mb-6">
-                                    {
-                                        active === 'profile' && (
-                                            <div className={'flex flex-col gap-5 w-full md:w-2/3'}>
-                                                <FilePond
-                                                    files={files}
-                                                    onupdatefiles={setFiles}
-                                                    allowMultiple={false}
-                                                    server={{
-                                                        url: `${API_BASE_URL}users/${user?.id}/image`
-                                                    }}
-                                                    name="thumb"
-                                                    labelIdle='Selectionnez une image'
-                                                />
-                                                <UpdateProfile user={user} />
-                                            </div>
-                                        )
-                                    }
-                                    {
-                                        active === 'solutions' && <SolutionCard solutions={solutions} />
-                                    }
-                                </div>
+                                <Tabs defaultValue="profile">
+                                    <TabsList className="flex items-center gap-2 w-[200px] bg-none">
+                                        <TabsTrigger value="profile">Profile</TabsTrigger>
+                                        <TabsTrigger value="solutions">Solutions</TabsTrigger>
+                                    </TabsList>
+                                    <TabsContent value="profile">
+                                        <div className={'flex flex-col gap-5 w-full md:w-2/3'}>
+                                            <FilePond
+                                                files={files}
+                                                onupdatefiles={setFiles}
+                                                allowMultiple={false}
+                                                server={{
+                                                    url: `${API_BASE_URL}users/${user?.id}/image`
+                                                }}
+                                                name="thumb"
+                                                labelIdle='Selectionnez une image'
+                                            />
+                                            <UpdateProfile user={user}/>
+                                        </div>
+                                    </TabsContent>
+                                    <TabsContent value="profile">
+                                        {
+                                            solutions && <SolutionCard solutions={solutions}/>
+                                        }
+                                    </TabsContent>
+                                </Tabs>
                             </>
                         ) : (
                             <>
                                 <div className="flex flex-col md:flex-row md:items-center gap-5 pt-16 mb-8">
-                                    <Skeleton className={'w-32 h-32 rounded-full'} />
+                                    <Skeleton className={'w-32 h-32 rounded-full bg-gray-200'}/>
                                     <div className="flex flex-col items-start gap-2">
-                                        <Skeleton className="h-8 w-[250px]" />
-                                        <Skeleton className="h-4 w-[250px]" />
-                                        <Skeleton className="h-4 w-[250px]" />
+                                        <Skeleton className="h-8 w-full md:w-[400px] bg-gray-200"/>
+                                        <Skeleton className="h-4 w-full md:w-[400px] bg-gray-200"/>
+                                        <Skeleton className="h-4 w-full md:w-[400px] bg-gray-200"/>
                                     </div>
                                 </div>
 
                                 <div className="flex gap-5 mb-6">
-                                    <Skeleton className="h-4 w-[250px]" />
+                                    <Skeleton className="h-6 w-full md:w-2/3 bg-gray-200"/>
                                 </div>
 
                                 <div className="mb-6 flex flex-col gap-4">
-                                    <Skeleton className="h-6 w-[250px]" />
-                                    <Skeleton className="h-6 w-[250px]" />
-                                    <Skeleton className="h-6 w-[250px]" />
-                                    <Skeleton className="h-6 w-[250px]" />
-                                    <Skeleton className="h-6 w-[250px]" />
-                                    <Skeleton className="h-6 w-[250px]" />
+                                    {
+                                        Array.from({length: 10}).map((_, i) => (
+                                            <Skeleton key={i} className="h-10 w-full md:w-2/3 bg-gray-200"/>
+                                        ))
+                                    }
                                 </div>
                             </>
                         )
                     }
                 </div>
-                <ToastContainer />
+                <ToastContainer/>
             </div>
         </>
     )
