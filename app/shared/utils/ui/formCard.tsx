5a43b73a193d4699b9bcb1e0a15a2be6d337a9a6
Wilfried Musanzi <musanziw.m@gmail.com>
2024-01-19 13:08:11 +0200

    style: set shadow to lg

--- app/shared/utils/ui/formCard.tsx
+++ app/shared/utils/ui/formCard.tsx
@@ -12,7 +12,7 @@ export function FormCard({title, handleSubmit, children}: FormCardProps) {
             <div className="container relative m-auto px-6 md:px-12 xl:px-40">
                 <div className="m-auto space-y-8 md:w-8/12 lg:w-6/12 xl:w-6/12 mt-10">
                     <div
-                        className="rounded-md border border-gray-100 bg-white shadow-sm shadow-gray-600/30 backdrop-blur-2xl">
+                        className="rounded-md border border-gray-100 bg-white shadow-lg shadow-gray-600/30 backdrop-blur-2xl">
                         <div className="p-8 py-12 sm:p-16">
                             <form onSubmit={handleSubmit} className="relative gap-6 flex flex-col justify-center">
                                 <h2 className="text-2xl font-extrabold mb-4">{title}</h2>
