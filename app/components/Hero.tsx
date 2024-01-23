7fd9a771cd26666f953684c661c6415650613a59
Wilfried Musanzi <musanziw.m@gmail.com>
2024-01-23 15:32:07 +0200

    refactor: use the useStore hook to get logged user

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,17 +1,15 @@
 'use client'
 
 import Link from "next/link";
-import {WEB_LOGIN, WEB_SOLUTIONS_SUBMIT} from "@/app/shared/config/links";
-import {useContext} from "react";
-import {AuthContext} from "@/app/shared/providers/authProvider";
+import useStore from "@/app/shared/hooks/useStore";
 
 export function Hero() {
-    const {isLoggedIn} = useContext(AuthContext)
+    const user = useStore.use.user()
 
     return (
         <div className="bg-hero bg-center bg-cover">
             <div
-                className={"p-8 h-full pt-32 pb-28 flex flex-col items-center justify-center bg-indigo-800/60 text-gray-50 mx-auto"}>
+                className={"p-8 h-full pt-36 pb-24 flex flex-col items-center justify-center bg-indigo-800/60 text-gray-50 mx-auto"}>
                 <div className={"flex flex-col justify-center items-start gap-6 mx-auto md:max-w-screen-lg"}>
                     <h2 className={`text-xl md:text-4xl text-gray-50 font-semibold mt-6 md:w-2/3`}>
                         Cartographie des solutions innovantes locales pour accélérer
@@ -23,7 +21,7 @@ export function Hero() {
                         accélérer l&apos;atteinte des Objectifs de Développement Durable
                         (ODD).
                     </p>
-                    <Link href={isLoggedIn ? WEB_SOLUTIONS_SUBMIT : WEB_LOGIN}
+                    <Link href={user ? '/solutions/submit' : '/login'}
                           className={"px-6 text-lg fade-in-3 py-2 inline-block mb-10 rounded-md bg-indigo-500 text-white font-semibold"}>
                         Postulez dès maintenant
                     </Link>
