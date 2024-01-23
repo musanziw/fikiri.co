b81bd1bae3acd086f8505a5f98bde2d42a49f658
Wilfried Musanzi <musanziw.m@gmail.com>
2024-01-23 15:26:29 +0200

    refactor: move axios.ts to api.ts

---
+++ app/shared/config/api.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const apiBaseURL: string = 'http://localhost:8000/'
+
+const api = axios.create({
+    baseURL: apiBaseURL,
+    headers: {
+        "Content-Type": "application/json",
+    },
+    withCredentials: true,
+});
+
+export {api, apiBaseURL};
