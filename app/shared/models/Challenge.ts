c8f9648d1dec97ea618f2f178b9a442895ca86d6
Wilfried Musanzi <musanziw.m@gmail.com>
2024-01-23 15:27:56 +0200

    feat: add call challenge type

---
+++ app/shared/models/Challenge.ts
@@ -0,0 +1,11 @@
+import {Solution} from "@/app/shared/models/Solution";
+import {Thematic} from "@/app/shared/models/Thematic";
+
+export interface Challenge {
+    id: number
+    name: string
+    createdAt: Date
+    updatedAt: Date
+    solutions?: Solution[]
+    thematics?: Thematic[]
+}
