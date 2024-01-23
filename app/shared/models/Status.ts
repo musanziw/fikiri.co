9bc8d82479b6f46b798b48bf16e2a509de881a16
Wilfried Musanzi <musanziw.m@gmail.com>
2024-01-23 15:28:54 +0200

    feat: add image status type

---
+++ app/shared/models/Status.ts
@@ -0,0 +1,9 @@
+import {Solution} from "@/app/shared/models/Solution";
+
+export interface Status {
+    id: number,
+    name: string
+    createdAt: Date
+    updatedAt: Date
+    solutions?: Solution[]
+}
