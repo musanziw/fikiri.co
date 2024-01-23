48a89322089c88b0fb496f33e61ebc682c8930f9
Wilfried Musanzi <musanziw.m@gmail.com>
2024-01-23 15:34:08 +0200

    feat: clean up code and use React query

--- app/(pages)/login/page.tsx
+++ app/(pages)/login/page.tsx
@@ -1,101 +1,78 @@
 "use client";
 
-import {FormEvent, useContext, useMemo, useState} from "react";
+import {FormEvent} from "react";
 import Link from "next/link";
-import {FormCard} from "@/app/shared/utils/ui/formCard";
-import Topbar from "@/app/shared/components/Topbar";
-import {API_BASE_URL, API_LOGIN, WEB_PROFILE, WEB_REGISTER} from "@/app/shared/config/links";
-import axios from "@/app/shared/config/axios";
-import {getInputError} from "@/app/shared/helpers/getInputError";
+import {FormCard} from "@/app/shared/utils/formCard";
+import Topbar from "@/app/shared/utils/Topbar";
 import {toast, Toaster} from "react-hot-toast";
-import {AuthContext} from "@/app/shared/providers/authProvider";
 import {useRouter} from "next/navigation";
 import {Label} from "@/app/shared/utils/ui/label";
 import {Input} from "@/app/shared/utils/ui/input";
 import {Button} from "@/app/shared/utils/ui/button";
-import {Loader2, Mail} from "lucide-react";
+import {Loader2} from "lucide-react";
 import Image from "next/image";
 import googleLogo from "@/public/googleLogo.svg"
+import useStore from "@/app/shared/hooks/useStore";
+import {useMutation} from "react-query";
+import {api, apiBaseURL} from "@/app/shared/config/api";
+import {login} from "@/app/(pages)/login/_requests";
+import {AxiosError} from "axios";
+import {User} from "@/app/shared/models/User";
 
 export default function Login() {
-    const [errors, setErrors] = useState([])
-    const [isPending, setIsPending] = useState(false)
-    const {setIsLoggedIn, setUser} = useContext(AuthContext)
+    const setUser = useStore.use.setUser()
     const router = useRouter()
 
-    async function handleSubmit(e: FormEvent) {
-        e.preventDefault();
-        setIsPending(true)
-        setErrors([])
+    const {isLoading, mutate} = useMutation(async (e: FormEvent) => {
+        e.preventDefault()
         const formData = new FormData(e.target as HTMLFormElement)
         const payload = Object.fromEntries(formData)
-        try {
-            const {data: response} = await axios.post(API_LOGIN, payload)
-            toast.success(response.message)
-            setUser(response.data)
-            setIsLoggedIn(true)
-            setTimeout(() => {
-                router.push(WEB_PROFILE)
-            }, 1000)
-        } catch (e: any) {
-            const data = e.response.data
-            if (typeof data.message === 'string') {
-                toast.error(data.message)
-            } else {
-                setErrors(data.message)
-            }
-            setIsLoggedIn(false)
+        if (!payload.email || !payload.password) throw new Error('Veuillez remplir tous les champs')
+        return await login(payload)
+    }, {
+        onSuccess: (data) => {
+            toast.success('Vous êtes connecté')
+            setUser(data)
+            router.push('/me')
+        },
+        onError: (error: AxiosError<any>) => {
+            const message: string = error.response?.data?.message || error.message
+            toast.error(message ?? 'Une erreur est survenue')
         }
-        setIsPending(false)
-    }
+    })
 
-    async function loginWithGoogle(e: FormEvent) {
-        e.preventDefault();
-        window.location.replace(`${API_BASE_URL}auth/google/redirect`);
-        try {
-            await axios.get('auth/login')
-        } catch {
-            router.push('/login')
-        }
+    const loginWithGoogle = async () => {
+        window.location.replace(`${apiBaseURL}auth/google/redirect`);
+        api.get('auth/login')
+            .catch(() => router.push('/login'))
     }
 
     return (
         <div className={'relative'}>
             <Topbar/>
-            <FormCard title={"Se connecter"} handleSubmit={handleSubmit}>
-
+            <FormCard title={"Se connecter"} handleSubmit={mutate}>
                 <Label htmlFor={'email'}>Email</Label>
-                <Input name={'email'} placeholder={'Entrez votre email'} type={'text'}
-                       error={getInputError(errors, 'email')}/>
-
+                <Input name={'email'} placeholder={'Entrez votre email'}/>
                 <Label htmlFor={'password'}>Mot de passe</Label>
-                <Input name={'password'} placeholder={'Entrez votre mot de passe'}
-                       type={'password'} error={getInputError(errors, 'password')}/>
-
-                <Button type={'submit'} disabled={isPending} className={'mt-5'}>
+                <Input name={'password'} type={'password'} placeholder={'Entrez votre mot de passe'}/>
+                <Button type={'submit'} disabled={isLoading} className={'mt-5'}>
                     {
-                        isPending ? (
+                        isLoading ? (
                             <>
                                 <Loader2 className="mr-2 h-4 w-4 animate-spin"/>
                                 En cours ...
                             </>
-                        ) : " Se connecter"
+                        ) : "Se connecter"
                     }
                 </Button>
-
-                <div className="flex flex-row gap-5 justify-center items-center">
-                    <div className="basis-1/2 border-t border-gray-300 text-sm text-gray-500"></div>
-                    <div className="pb-0">ou</div>
-                    <div className="basis-1/2 border-t border-gray-300 text-sm text-gray-500"></div>
-                </div>
-
-                <Button onClick={loginWithGoogle} variant={'outline'}>
+                <hr className="my-6 border-gray-300 w-full"/>
+                <Button onClick={loginWithGoogle} variant={'outline'} type={'button'}>
                     <Image src={googleLogo} alt={'img logo'} className="mr-2 h-4 w-4"/> Se connecter avec Google
                 </Button>
 
                 <p className="border-t border-gray-300 pt-6 text-sm text-gray-500">
                     Vous n&lsquo;avez pas de compte ?
-                    <Link href={WEB_REGISTER} className="text-gray-950 inline-block ml-1">
+                    <Link href={'/register'} className="text-gray-950 inline-block ml-1">
                         Inscrivez-vous
                     </Link>
                 </p>
