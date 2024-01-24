6cf8da889a98e38ba9a9e00f8cd5417fee08e309
Wilfried Musanzi <musanziw.m@gmail.com>
2024-01-24 14:33:40 +0200

    refactor: add solutions related request

---
+++ app/(pages)/solutions/submit/_requests.ts
@@ -0,0 +1,26 @@
+import {api} from "@/app/shared/config/api";
+import {Call} from "@/app/shared/models/Call";
+import {Thematic} from "@/app/shared/models/Thematic";
+import {Challenge} from "@/app/shared/models/Challenge";
+
+const loadCalls = async () => {
+    const {data} = await api.get('calls')
+    return data.data as Call[]
+}
+
+const loadThematics = async (id: number) => {
+    const {data} = await api.get(`calls/${id}`)
+    return data.data.thematics as Thematic[]
+}
+
+const loadChallenges = async (id: number) => {
+    const {data} = await api.get(`thematics/${id}`)
+    return data.data.challenges as Challenge[]
+}
+
+const submitSolution = async <T>(payload: T) => {
+    const response = await api.post('solutions', payload);
+    return response.data
+}
+
+export {loadCalls, loadThematics, loadChallenges, submitSolution}
