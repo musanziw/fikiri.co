4082e7f8309f0adf35370319211649fe3728f85c
Wilfried Musanzi <musanziw.m@gmail.com>
2024-01-24 14:39:32 +0200

    feat: use React Query

--- app/(pages)/solutions/submit/page.tsx
+++ app/(pages)/solutions/submit/page.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import {FormEvent, useEffect, useState} from "react";
+import React, {FormEvent, useEffect, useState} from "react";
 import {toast, Toaster} from "react-hot-toast";
 import {useRouter} from "next/navigation";
-import Select from "react-select";
 import Topbar from "@/app/shared/utils/Topbar";
 import {FormCard} from "@/app/shared/utils/formCard";
 import {Input} from "@/app/shared/utils/ui/input";
@@ -12,138 +11,115 @@ import {Textarea} from "@/app/shared/utils/ui/textarea";
 import {Button} from "@/app/shared/utils/ui/button";
 import {Loader2} from "lucide-react";
 import {getInputError} from "@/app/shared/helpers/getInputError";
-import {Call} from "@/app/shared/models/Call";
+import useStore from "@/app/shared/hooks/useStore";
+import {useMutation, useQuery} from "react-query";
+import {loadCalls, loadChallenges, loadThematics, submitSolution} from "@/app/(pages)/solutions/submit/_requests";
+import {AxiosError} from "axios";
 import {Thematic} from "@/app/shared/models/Thematic";
 import {Challenge} from "@/app/shared/models/Challenge";
-import useStore from "@/app/shared/hooks/useStore";
-import {api} from "@/app/shared/config/api";
+import Select, {MultiValue, SingleValue} from "react-select";
+import {Call} from "@/app/shared/models/Call";
+
+interface OptionProps {
+    value: number;
+    label: string;
+}
 
 export default function SubmitProject() {
-    const [calls, setCalls] = useState<Call[]>();
-    const [thematics, setThematics] = useState<Thematic[]>()
-    const [selectedCall, setSelectedCall] = useState<any>()
-    const [selectedThematic, setSelectedThematic] = useState<string>('')
-    const [challenges, setChallenges] = useState<Challenge[]>()
-    const [selectedChallenges, setSelectedChallenges] = useState<Challenge>()
+    const [selectedCall, setSelectedCall] = useState<number | undefined>(undefined)
+    const [selectedThematic, setSelectedThematic] = useState<number | undefined>(undefined)
+    const [selectedChallenges, setSelectedChallenges] = useState<number[]>([])
+    const [errors, setErrors] = useState<ApiValidationError[]>([])
+    const [thematics, setThematics] = useState<OptionProps[]>([])
+    const [challenges, setChallenges] = useState<OptionProps[]>([])
     const router = useRouter();
     const user = useStore.use.user()
-    const [isPending, setIsPending] = useState<boolean>(false)
-    const [errors, setErrors] = useState([])
+
+    const {data: calls} = useQuery(['calls'], async () => loadCalls().then((data) => data.map((call: Call) => ({
+            value: call.id,
+            label: call.name
+        })))
+    )
 
     useEffect(() => {
-        (async () => {
-            const {data: response} = await api.get('calls')
-            const options = response.data
-            setCalls(
-                options?.map((option: any) => ({
-                    value: option.id,
-                    label: option.name,
-                }))
-            );
-        })()
-    }, []);
-
-    const handleCallChange = async (option: any) => {
-        setSelectedCall(option.value)
-        const {data: response} = await api.get(`calls/${option.value}`)
-        const {thematics: options} = response.data
-        setThematics(
-            options.map((option: any) => ({
-                value: option.id,
-                label: option.name,
-            }))
-        );
-    };
-
-    const handleThematicsChange = async (option: any) => {
-        setSelectedThematic(option.value)
-        const {data: reponse} = await api.get(`thematics/${option.value}`)
-        const {challenges: options} = reponse.data
-        setChallenges(
-            options.map((option: any) => ({
-                value: option.id,
-                label: option.name,
-            }))
-        );
-    }
-
-    function handleChallenge(options: any) {
-        setSelectedChallenges(
-            options.map((option: any) => option.value)
-        )
-    }
-
-    const handleSubmit = async (e: FormEvent) => {
-        e.preventDefault();
-        setIsPending(true)
-        const formData = new FormData(e.target as HTMLFormElement)
-        const data = Object.fromEntries(formData)
-        const payload = {
-            ...data,
-            user: user?.email,
-            call: selectedCall,
-            thematic: selectedThematic,
-            challenges: selectedChallenges,
+        if (selectedCall) {
+            loadThematics(selectedCall).then((data) => {
+                setThematics(data.map((thematic: Thematic) => ({
+                    value: thematic.id,
+                    label: thematic.name
+                })))
+            })
         }
-        try {
-            await api.post('solutions', JSON.stringify(payload));
-            toast.success("Solution soumis avec succès !");
-            setTimeout(() => {
-                router.back()
-            }, 1000)
-        } catch (e: any) {
-            const data = e.response.data
-            if (typeof data.message === 'string') {
-                toast.error(data.message)
-            } else {
-                setErrors(data.message)
+    }, [selectedCall])
+
+    useEffect(() => {
+        if (selectedThematic) {
+            loadChallenges(selectedThematic).then((data) => {
+                setChallenges(data.map((challenge: Challenge) => ({
+                    value: challenge.id,
+                    label: challenge.name
+                })))
+            })
+        }
+    }, [selectedThematic])
+
+    const handleCallChange = async (option: SingleValue<OptionProps>) => {
+        option && setSelectedCall(option.value)
+    }
+
+    const handleThematicsChange = async (option: SingleValue<OptionProps>) => {
+        option && setSelectedThematic(option.value)
+    }
+
+    const handleChallenge = async (options: MultiValue<OptionProps>) => setSelectedChallenges(
+        options.map((option: OptionProps) => option.value)
+    )
+
+    const {mutate, isLoading} = useMutation(async (e: FormEvent) => {
+            e.preventDefault();
+            setErrors([])
+            const formData = new FormData(e.target as HTMLFormElement)
+            const data = Object.fromEntries(formData)
+            const payload = {
+                ...data,
+                user: user?.email,
+                call: selectedCall,
+                thematic: selectedThematic,
+                challenges: selectedChallenges,
+            }
+            return await submitSolution(payload)
+        }, {
+            onSuccess: () => {
+                toast.success('Solution soumise avec succès')
+                router.push('/me')
+            },
+            onError: (error: AxiosError<any>) => {
+                const message: string | ApiValidationError[] = error?.response?.data?.message
+                if (Array.isArray(message)) setErrors(message)
+                else toast.error(message)
             }
         }
-        setIsPending(false)
-    };
+    )
 
     return (
         <div className={'relative'}>
             <Topbar/>
-            <FormCard handleSubmit={handleSubmit} title={'Votre solution'}>
+            <FormCard handleSubmit={mutate} title={'Soumettre la solution'}>
                 <Label htmlFor={'name'}>Nom de la solution</Label>
                 <Input name={'name'} placeholder={"Saisir le nom de votre solution"}
                        error={getInputError(errors, 'name')} type={'text'}/>
                 {
                     calls && (
                         <>
-                            <div className="flex flex-col gap-3">
-                                <label htmlFor={'call'}>Selectionner l&lsquo;appel</label>
-                                <Select
-                                    id={'call'}
-                                    name={'call'}
-                                    options={calls}
-                                    onChange={handleCallChange}
-                                    className="h-12 rounded w-full mt-2 basic-select"
-                                />
-                            </div>
+                            <Label htmlFor={'call'}>Selectionner l&apos;appel</Label>
+                            <Select id={'call'} name={'call'} options={calls} onChange={handleCallChange}/>
 
-                            <div className="flex flex-col gap-3">
-                                <label htmlFor={"thematic"}>Choisir une thématique</label>
-                                <Select
-                                    id={'thematic'}
-                                    options={thematics}
-                                    onChange={handleThematicsChange}
-                                    className="h-12 rounded w-full mt-2 basic-multi-select"
-                                />
-                            </div>
+                            <Label htmlFor={"thematic"}>Choisir une thématique</Label>
+                            <Select id={'thematic'} options={thematics} onChange={handleThematicsChange}/>
 
-                            <div className="flex flex-col gap-3">
-                                <label htmlFor={'challenges'}>A quoi votre solution répond elle ?</label>
-                                <Select
-                                    id={'challenges'}
-                                    name={'challenges'}
-                                    isMulti
-                                    options={challenges}
-                                    onChange={handleChallenge}
-                                    className="h-12 rounded w-full mt-2 basic-multi-select"
-                                />
-                            </div>
+                            <Label htmlFor={'challenges'}>A quoi votre solution répond elle ?</Label>
+                            <Select id={'challenges'} name={'challenges'} isClearable={false} isMulti options={challenges} onChange={handleChallenge}/>
+
                         </>
                     )
                 }
@@ -161,9 +137,9 @@ export default function SubmitProject() {
                        error={getInputError(errors, 'videoLink')}
                        type={'text'}/>
 
-                <Button type={'submit'} disabled={isPending} className={'mt-5'}>
+                <Button type={'submit'} disabled={isLoading} className={'mt-5'}>
                     {
-                        isPending ? (
+                        isLoading ? (
                             <>
                                 <Loader2 className="mr-2 h-4 w-4 animate-spin"/>
                                 Soumission...
