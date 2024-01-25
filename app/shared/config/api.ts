7108e47060a9aa6d139218cfd72d1bb85e2b6f9f
Wilfried Musanzi <musanziw.m@gmail.com>
2024-01-25 15:10:54 +0200

    fix: fix api url link

--- app/shared/config/api.ts
+++ app/shared/config/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiBaseURL: string = 'http://localhost:8000/'
+const apiBaseURL: string = 'https://api.fikiri-solutions.co/'
 
 const api = axios.create({
     baseURL: apiBaseURL,
