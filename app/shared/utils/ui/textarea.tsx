2bcee3bdfdd11cd68e82a1f89a81548ae2fd3ba2
Wilfried Musanzi <musanziw.m@gmail.com>
2024-01-26 11:38:20 +0200

    style: add height

--- app/shared/utils/ui/textarea.tsx
+++ app/shared/utils/ui/textarea.tsx
@@ -13,7 +13,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
             <div className={'flex flex-col gap-1'}>
                 <textarea
                     className={cn(
-                        "flex min-h-[100px] w-full rounded-md border border-input bg-background px-3 py-5 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+                        "flex min-h-[200px] w-full rounded-md border border-input bg-background px-3 py-5 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
                         className
                     )}
                     ref={ref}
