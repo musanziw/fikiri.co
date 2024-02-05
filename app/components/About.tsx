0ff93fc11c026fbcd5dddb527fa24f6d22e6b347
Wilfried Musanzi <musanziw.m@gmail.com>
2024-02-05 15:30:29 +0200

    style: reduce logos size and drop margin left

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -133,19 +133,18 @@ export function About() {
                                 <Image
                                     src={partainer.src}
                                     alt={partainer.nom}
-                                    className={"ml-2 w-40"}
+                                    className={"w-36"}
                                 />
                             </li>
                         ))}
                     </ul>
-                    <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
-                        aria-hidden="true">
+                    <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll" aria-hidden="true">
                         {PARTAINERS.map((partainer, index) => (
                             <li key={index}>
                                 <Image
                                     src={partainer.src}
                                     alt={partainer.nom}
-                                    className={"ml-2 w-40"}
+                                    className={"w-36"}
                                 />
                             </li>
                         ))}
