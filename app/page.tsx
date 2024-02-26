4d561815bd4d2cdf40565c56d4084e30eb01a207
Musanzi Wilfried <musanziw.m@gmail.com>
2024-02-26 16:45:58 +0200

    feat: add solutions design for home

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 import { Hero } from "@/app/(components)/Hero";
 import { About } from "@/app/(components)/About";
 import { Footer } from "@/app/shared/utils/Footer";
-import Sdg from "@/app/(components)/Sdg";
 import Topbar from "@/app/shared/utils/Topbar";
+import Solutions from "./(components)/Solutions";
 
 export default function Home() {
   return (
     <main className={"relative"}>
       <Topbar />
       <Hero />
-      <Sdg />
+      <Solutions />
       <About />
       <Footer />
     </main>
