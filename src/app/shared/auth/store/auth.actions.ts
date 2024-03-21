2c46788afe5b6655ae56ff480c08f35a392a3b92
Musanzi Wilfried <musanziw.m@gmail.com>
2024-03-21 15:05:42 +0200

    feat: add error

--- src/app/shared/auth/store/auth.actions.ts
+++ src/app/shared/auth/store/auth.actions.ts
@@ -1,13 +1,12 @@
-import { createAction, props } from '@ngrx/store';
-import { User } from '../../types/models-interfaces';
+import {createAction, props} from '@ngrx/store';
+import {User} from '../../types/models-interfaces';
 
 export const authentication = createAction('[Auth] authentication');
-
 export const authenticationSuccess = createAction(
   '[Auth] authentication success',
   props<User>()
 );
-
 export const authenticationFailure = createAction(
-  '[Auth] authentication failure'
+  '[Auth] authentication failure',
+  props<{ error: string }>()
 );
--- src/app/shared/auth/store/auth.effects.ts
+++ src/app/shared/auth/store/auth.effects.ts
@@ -1,20 +1,23 @@
-import { Injectable } from '@angular/core';
-import { Actions, createEffect, ofType } from '@ngrx/effects';
+import {Injectable} from '@angular/core';
+import {Actions, createEffect, ofType} from '@ngrx/effects';
 import * as authActions from './auth.actions';
-import { catchError, map, mergeMap, of } from 'rxjs';
-import { AuthService } from '../auth.service';
+import {catchError, map, mergeMap, of} from 'rxjs';
+import {AuthService} from '../auth.service';
 
 @Injectable()
 export class AuthEffects {
-  constructor(private authService: AuthService, private actions$: Actions) {}
+  constructor(private authService: AuthService, private actions$: Actions) {
+  }
 
-  isAuth$ = createEffect(() => {
+  authentication$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(authActions.authentication),
       mergeMap(() => {
         return this.authService.authenticatedUser().pipe(
           map((res) => authActions.authenticationSuccess(res.data)),
-          catchError(() => of(authActions.authenticationFailure()))
+          catchError((err) =>
+            of(authActions.authenticationFailure({error: err.error.message}))
+          )
         );
       })
     );
