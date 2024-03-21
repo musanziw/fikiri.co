847f4fd3e73479e409df9b9ee1f003365f3bd5f3
Musanzi Wilfried <musanziw.m@gmail.com>
2024-03-21 15:05:13 +0200

    feat: add  winning solutions and solutions state

--- src/app/shared/types/app-store.interface.ts
+++ src/app/shared/types/app-store.interface.ts
@@ -1,5 +1,9 @@
-import { AuthStoreInterface } from '../auth/types/auth-store.interface';
+import {WinningSolutionsStoreInterface} from '../../home/ui/winning-solutions/types/winning-solutions-store.interface';
+import {SolutionsStoreInterface} from '../../mapped-solutions/solutions/types/solutions-store.interface';
+import {AuthStoreInterface} from '../auth/types/auth-store.interface';
 
 export interface AppStoreInterface {
   auth: AuthStoreInterface;
+  winningSolutions: WinningSolutionsStoreInterface;
+  solutions: SolutionsStoreInterface;
 }
