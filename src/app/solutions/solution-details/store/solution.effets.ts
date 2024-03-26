6b267d8f2136a20878bf33d02b828c76be69bff1
Musanzi Wilfried <musanziw.m@gmail.com>
2024-03-26 09:09:35 +0200

    refactor: clean up code

--- src/app/solutions/solution-details/store/solution.effets.ts
+++ src/app/solutions/solution-details/store/solution.effets.ts
@@ -1,8 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Actions, createEffect, ofType} from '@ngrx/effects';
-import {catchError, map, mergeMap, of, switchMap} from 'rxjs';
+import {catchError, map, mergeMap, of} from 'rxjs';
 import {SolutionService} from '../solution.service';
-import {ActivatedRoute} from '@angular/router';
 import {solutionActions} from "./solution.actions";
 
 @Injectable()
@@ -10,7 +9,6 @@ export class SolutionEffect {
   constructor(
     private solutionService: SolutionService,
     private actions$: Actions,
-    private route: ActivatedRoute
   ) {
   }
 
@@ -18,19 +16,9 @@ export class SolutionEffect {
     return this.actions$.pipe(
       ofType(solutionActions.load),
       mergeMap(() => {
-        return this.route.paramMap.pipe(
-          switchMap((params) => {
-            // const id = params.get('id');
-            // console.log('id', id);
-            return this.solutionService.getSolution(1).pipe(
-              map((res) =>
-                solutionActions.loadSuccess({solution: res.data})
-              ),
-              catchError((error) =>
-                of(solutionActions.loadFailure({error: error.message}))
-              )
-            );
-          })
+        return this.solutionService.getSolution(1).pipe(
+          map((res) => solutionActions.loadSuccess({solution: res.data})),
+          catchError((error) => of(solutionActions.loadFailure({error: error.message})))
         );
       })
     );
--- src/app/solutions/solutions-list/solutions.service.ts
+++ src/app/solutions/solutions-list/solutions.service.ts
@@ -1,13 +1,14 @@
-import { Injectable } from '@angular/core';
-import { Solution } from '../../shared/types/models-interfaces';
-import { HttpClientService } from '../../shared/services/http-client.service';
-import { Observable, Subscription } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {Solution} from '../../shared/types/models-interfaces';
+import {HttpClientService} from '../../shared/services/http-client.service';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SolutionsService {
-  constructor(private http: HttpClientService) {}
+  constructor(private http: HttpClientService) {
+  }
 
   getMappedSolutions(cursor: number): Observable<{ data: Solution[] }> {
     return this.http.get<Solution[]>(`solutions/mapped/all?cursor=${cursor}`);
