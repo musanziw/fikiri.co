bb4ebdff87a42790d2fb4af946219e01546944ae
Musanzi Wilfried <musanziw.m@gmail.com>
2024-04-12 08:23:44 +0200

    feat: add component store

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy} from '@angular/core';
+import {Component} from '@angular/core';
 import {FormCardComponent} from '../../shared/components/form-card/form-card.component';
 import {ButtonComponent} from '../../shared/ui/button/button.component';
 import {InputComponent} from '../../shared/ui/input/input.component';
@@ -7,12 +7,10 @@ import {AsyncPipe, NgIf, NgOptimizedImage} from '@angular/common';
 import {ButtonOutlineComponent} from '../../shared/ui/button-outline/button-outline.component';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators,} from '@angular/forms';
 import {ToastContainerDirective} from 'ngx-toastr';
-import {select, Store} from '@ngrx/store';
-import {AuthStoreInterface} from '../types/auth-store.interface';
 import {environment} from "../../../environments/environment";
 import {Observable} from "rxjs";
-import {authActions} from "../store/auth.actions";
-import {selectAuthState} from "../store/auth.reducers";
+import {LoginStore} from "./data-access/login.store";
+import {LoginStoreInterface} from "./types/login-store.interface";
 
 @Component({
   selector: 'fk-login',
@@ -29,30 +27,27 @@ import {selectAuthState} from "../store/auth.reducers";
     AsyncPipe,
     NgIf,
   ],
+  providers: [LoginStore],
   templateUrl: './login.component.html',
 })
-export class LoginComponent implements OnDestroy {
+export class LoginComponent {
   form: FormGroup;
   apiUrl: string = environment.apiUrl
-  state$: Observable<AuthStoreInterface>
+  vm$: Observable<LoginStoreInterface>
 
-  constructor(private store: Store, private formBuilder: FormBuilder) {
+  constructor(private store: LoginStore, private formBuilder: FormBuilder) {
     this.form = this.formBuilder.group({
       email: ['', Validators.required],
       password: ['', Validators.required],
     });
-    this.state$ = this.store.pipe(select(selectAuthState))
+    this.vm$ = this.store.vm$
   }
 
   onSubmit(): void {
-    this.store.dispatch(authActions.login({payload: this.form.value}))
+    this.store.login(this.form.value)
   }
 
   loginWithGoogle(): void {
     window.location.replace(`${this.apiUrl}auth/google/redirect`);
-  }
-
-  ngOnDestroy(): void {
-    this.store.dispatch(authActions.deleteError());
   }
 }
