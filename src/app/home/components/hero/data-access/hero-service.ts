02b70ccee85930263361db3e6a5e52907e601b44
Musanzi Wilfried <musanziw.m@gmail.com>
2024-04-17 13:43:19 +0200

    feat: display fikiri stats count users and solutions

---
+++ src/app/home/components/hero/data-access/hero-service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from "@angular/core";
+import {HttpClientService} from "../../../../shared/services/http-client.service";
+import {TotalsInterface} from "../types/totals.interface";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+  constructor(private httpClient: HttpClientService) {
+  }
+
+  getCount(): Observable<TotalsInterface> {
+    return this.httpClient.get<TotalsInterface>('dashboard');
+  }
+}
---
+++ src/app/home/components/hero/types/hero-store.interfce.ts
@@ -0,0 +1,7 @@
+import {TotalsInterface} from "./totals.interface";
+
+export interface HeroStoreInterfce {
+  isLoading: boolean;
+  totals: TotalsInterface | null;
+  error: string | null;
+}
