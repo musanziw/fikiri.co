77045655dba8ab611fa67dff512a10dcb8df3f3c
Musanzi Wilfried <musanziw.m@gmail.com>
2024-04-17 13:45:30 +0200

    style: change style

--- src/app/home/components/hero/hero.component.ts
+++ src/app/home/components/hero/hero.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {AsyncPipe, NgIf, NgOptimizedImage} from "@angular/common";
+import {HeroStore} from "./data-access/hero.store";
+import {Observable} from "rxjs";
+import {HeroStoreInterfce} from "./types/hero-store.interfce";
+import {TopbarComponent} from "../../../shared/components/topbar/topbar.component";
 
 @Component({
   selector: 'component-hero',
   standalone: true,
-  imports: [
-    RouterLink
-  ],
+  imports: [RouterLink, NgOptimizedImage, NgIf, AsyncPipe, TopbarComponent],
+  providers: [HeroStore],
   templateUrl: './hero.component.html',
 })
-export class HeroComponent {
+export class HeroComponent implements OnInit {
+  vm$: Observable<HeroStoreInterfce>
 
+  constructor(private store: HeroStore) {
+    this.vm$ = this.store.vm$;
+  }
+
+  ngOnInit(): void {
+    this.store.getTotals()
+  }
 }
