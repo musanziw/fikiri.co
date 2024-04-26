aa2b188f67d261e671b6ee77d1192cecb7795f04
Musanzi Wilfried <musanziw.m@gmail.com>
2024-04-26 10:12:53 +0200

    chore: import message component

--- src/app/shared/components/form-card/form-card.component.ts
+++ src/app/shared/components/form-card/form-card.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MessageComponent } from '../message/message.component';
 
 @Component({
   selector: 'app-form-card',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './form-card.component.html',
+  imports: [CommonModule, MessageComponent]
 })
 export class FormCardComponent {
   @Input() formTitle: string = '';
