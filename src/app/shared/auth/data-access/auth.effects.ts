8fcccbdcb6600e86585319b7bddbc7459f7a5b73
Musanzi Wilfried <musanziw.m@gmail.com>
2024-04-26 10:11:17 +0200

    chore: drop tap on logout and use exhaustMap instead

--- src/app/shared/auth/data-access/auth.effects.ts
+++ src/app/shared/auth/data-access/auth.effects.ts
@@ -1,6 +1,6 @@
 import { inject } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { catchError, map, mergeMap, of, tap } from 'rxjs';
+import { catchError, exhaustMap, map, of } from 'rxjs';
 import { AuthService } from './auth.service';
 import { authActions } from './auth.actions';
 import { Router } from '@angular/router';
@@ -9,23 +9,29 @@ export const authenticateEffect = createEffect(
   (actions$ = inject(Actions), authService = inject(AuthService)) =>
     actions$.pipe(
       ofType(authActions.authentication),
-      mergeMap(() =>
+      exhaustMap(() =>
         authService.authenticatedUser().pipe(
           map((user) => authActions.authenticateUser({ user })),
-          catchError(() => of()),
-        ),
-      ),
+          catchError(() => of())
+        )
+      )
     ),
-  { functional: true },
+  { functional: true }
 );
 
 export const logoutEffect = createEffect(
   (actions$ = inject(Actions), authService = inject(AuthService), router = inject(Router)) =>
     actions$.pipe(
       ofType(authActions.logout),
-      mergeMap(() => authService.logout().pipe(map(() => authActions.logout()))),
-      tap(() => router.navigate(['/'])),
-      catchError(() => of()),
+      exhaustMap(() =>
+        authService.logout().pipe(
+          map(() => {
+            router.navigate(['/']);
+            return authActions.logout();
+          })
+        )
+      ),
+      catchError(() => of())
     ),
-  { functional: true },
+  { functional: true }
 );
