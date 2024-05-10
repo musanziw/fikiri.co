cb6cfe59dcab58589acc0fa1189bc58634e6737c
Musanzi Wilfried <musanziw.m@gmail.com>
2024-05-10 15:13:58 +0200

    fix: find winning solutions

--- src/app/home/components/winning-solutions/data-access/winning-solutions.service.ts
+++ src/app/home/components/winning-solutions/data-access/winning-solutions.service.ts
@@ -10,6 +10,6 @@ export class WinningSolutionsService {
   constructor(private http: HttpClientService) {}
 
   getWinningSolutions(): Observable<Solution[]> {
-    return this.http.get<Solution[]>('solutions/mapped?cursor=1');
+    return this.http.get<Solution[]>('solutions/winning-solutions');
   }
 }
